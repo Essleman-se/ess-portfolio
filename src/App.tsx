08989d6bd130f61de4ee1e5f9e5b57a99f0621a3
Essleman-se <seydo.ess@gmail.com>
2025-10-12 14:23:23 -0700

    start and serve script changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ const App = () => {
   return (
     <>
       <div className='text-red-400 m-2'>App</div>
-      <p className='text-gray-700 text-1xl'>Your Tailwind css setup is successfull</p>
-      <p className='text-1xl font-bold text-blue-500'>Hello, Tailwind CSS!</p>
+      <p className='text-gray-700 text-1xl'>Tailwind css setup</p>
+      <p className='text-1xl font-bold text-blue-500'>Hello</p>
     </>
   )
 }
