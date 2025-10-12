0e008f077d5c10056213cadd5d7bb43e57c96fec
Essleman-se <seydo.ess@gmail.com>
2025-10-12 08:50:42 -0700

    My first Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div className='text-red-400 m-2'>App</div>
+      <p className='text-gray-700 text-1xl'>Your Tailwind css setup is successfull</p>
+      <p className='text-1xl font-bold text-blue-500'>Hello, Tailwind CSS!</p>
+    </>
+  )
+}
+
+export default App
