3b2a16019a1e031d00705a762bc03e8522171bd8
Essleman-se <seydo.ess@gmail.com>
2025-10-27 20:53:11 -0700

    components and section created and changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import "./App.css";
+import "./index.css";
+import { LoadingScreen } from "./components/LoadingScreen";
+import { useState } from "react";
+import { NavBar } from "./components/NavBar";
+import { MobileMenu } from "./components/MobileMenu";
 
-const App = () => {
+function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] =useState(false);
+
   return (
     <>
-      <div className='text-red-400 m-2'>App</div>
-      <p className='text-gray-700 text-1xl'>Tailwind css setup</p>
-      <p className='text-1xl font-bold text-blue-500'>Hello</p>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />} {" "}
+        <div className={`min-h-screen transition-opacity duration-700 
+          ${isLoaded ? 'opacity-100' : 'opacity-0'} bg-black text-gray-100`}>
+            <NavBar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+            <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+        </div>
     </>
   )
 }
